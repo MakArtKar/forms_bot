0c3aabb0d557663a8ec08a7b3e9c5bebd3e2d9c6
Artem <makoyan2001@icloud.com>
2020-04-06 15:26:53 +0400

    echo-bot

---
+++ config.py
@@ -0,0 +1,1 @@
+token = '1023651188:AAGD4KcqKVGfjz8QzU1JOGgPk3PzviEvx-E'
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from token import TOKEN
+import telebot
+import config
 
-print(TOKEN)
+
+bot = telebot.TeleBot(config.token)
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.infinity_polling()
