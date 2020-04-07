d32fb00c36776f08a7ceb6e98241a67b498dcb99
Artem <makoyan2001@icloud.com>
2020-04-07 14:24:45 +0400

    no

--- main.py
+++ main.py
@@ -11,6 +11,12 @@ def find_file_ids(message):
     		f = open('music/'+file, 'rb')
     		msg = bot.send_voice(message.chat.id, f, NONE)
     		bot.send_message(message.chat.id, msg.voice.file_id, reply_to_message_id=msg.message_id)
+    	time.sleep(3)
+
+@bot.message_handler(content_types=['text'])
+def find_file_ids(message):
+    bot.send_message(message.chat.id, message.text)
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
