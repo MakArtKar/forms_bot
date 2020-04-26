7a94743524405c8d5b5d97eca590fe8b8638b211
sizovk <sizovki@yandex.ru>
2020-04-26 19:53:46 +0300

    added user class and improved message access

--- db_worker.py
+++ db_worker.py
@@ -1,4 +1,15 @@
+from datetime import datetime
+import logging
 import sqlite3
+
+class User:
+	
+	def __init__(self, chat_id, state = 0, current_form = None, current_question = None, forms_number = 0):
+		self.chat_id = chat_id
+		self.state = state
+		self.current_form = current_form
+		self.current_question = current_question
+		self.forms_number = forms_number
 
 class SQLighter:
 
@@ -13,12 +24,13 @@ class SQLighter:
 			state int,
 			current_form text,
 			current_question int,
+			forms_number int
 		""")
-		self.cursor.execute("""CREATE TABLE IF NOT EXISTS Questions(
+		self.cursor.execute("""CREATE TABLE IF NOT EXISTS QMessages(
 			form_id text,
 			question_id int,
 			message_id int,
-			message_ord int
+			inserted_time timestamp
 		""")
 		self.cursor.execute("""CREATE TABLE IF NOT EXISTS Answers(
 			chat_id int,
@@ -28,27 +40,42 @@ class SQLighter:
 		""")
 		self.connection.commit()
 
-	def insert_user(self, chat_id, state, current_form, current_question):
-		self.cursor.execute("INSERT INTO Users(chat_id, state, current_form, current_question) VALUES(?, ?, ?, ?)", (chat_id, state, current_form, current_question))
+	def insert_user(self, user):
+		self.cursor.execute("INSERT INTO Users(chat_id, state, current_form, current_question) VALUES(?, ?, ?, ?)", (user.chat_id, user.state, user.current_form, user.current_question, user.forms_number))
 		self.connection.commit()
 
-	def insert_question(self, form_id, question_id, message_id, message_ord):
-		self.cursor.execute("INSERT INTO Questions(form_id, question_id, message_id, message_ord) VALUES(?, ?, ?, ?)", (form_id, question_id, message_id, message_ord))
+	def delete_user(self, chat_id):
+		self.cursor.execute("DELETE FROM Users WHERE chat_id=?", (chat_id,))
 		self.connection.commit()
+
+	def get_user(self, chat_id):
+		users_by_chat_id = self.cursor.execute("SELECT chat_id, state, current_form, current_question, forms_number FROM monitoring_rss WHERE chat_id = ?", (chat_id,)).fetchall()
+		if not users_by_chat_id:
+			user = User(chat_id=chat_id)
+			insert_user(user)
+			return user
+		else:
+			if len(users_by_chat_id) > 1:
+				logging.error("More than one user with one chat_id in database")
+			user_tuple = users_by_chat_id[0]
+			user = User(chat_id=user_tuple[0], state=user_tuple[1], current_form=user_tuple[2], current_question=user_tuple[3], forms_number=user_tuple[4])
+			return user
+
+	def update_user(self, user):
+		self.delete_user(user.chat_id)
+		self.connection.commit()
+
+	def insert_message_to_question(self, form_id, question_id, message_id):
+		self.cursor.execute("INSERT INTO Questions(form_id, question_id, message_id, inserted_time) VALUES(?, ?, ?, ?)", (form_id, question_id, message_id, datetime.now()))
+		self.connection.commit()
+
+	def get_messages_from_question(self, form_id, question_id):
+		messages = self.cursor.execute("SELECT message_id FROM Questions WHERE form_id=? and question_id=? ORDER BY inserted_time", (form_id, question_id))
+		return messages
 
 	def insert_answer(self, chat_id, form_id, question_id, answer_text):
 		self.cursor.execute("INSERT INTO Answers(chat_id, form_id, question_id, answer_text) VALUES(?, ?, ?, ?)", (chat_id, form_id, question_id, answer_text))
 		self.connection.commit()
-
-	def get_users_by_chat_id(self, chat_id):
-		users = self.cursor.execute("SELECT chat_id, state, current_form, current_question FROM monitoring_rss WHERE chat_id = ?", (chat_id,)).fetchall()
-		return users
-
-	def get_messages_for_question(self, form_id, question_id):
-		messages = self.cursor.execute("SELECT message_ord message_id FROM Questions WHERE form_id=? and question_id=?", (form_id, question_id))
-		messages.sort()
-		message_ids = [m[1] for m in messages]
-		return message_ids
 
 	def get_all_answers(self, form_id):
 		return self.cursor.execute("SELECT chat_id, question_id, answer_text FROM Answers WHERE form_id=?", (form_id,)).fetchall()
