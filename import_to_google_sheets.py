e29b1d977e9fb2d3b7c6dc293d540cd11f1662d2
Artem <makoyan2001@icloud.com>
2020-05-14 20:09:02 +0400

    added comment to import_to_google_sheets.py

--- import_to_google_sheets.py
+++ import_to_google_sheets.py
@@ -5,7 +5,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 CREDENTIALS_FILE = 'api_key.json'
 credentials = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, ['https://www.googleapis.com/auth/spreadsheets',
                                                                                  'https://www.googleapis.com/auth/drive'])
-# Черновой вариант
+# Черновой вариант (как минимум работает только для 26 столбцов)
 class Spreadsheet:
 
     def __init__(self, spreadsheet_id, service):
