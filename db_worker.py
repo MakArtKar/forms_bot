44c49336ed9b3539649b9cf8ca9e4a6d26b192d9
Artem <makoyan2001@icloud.com>
2020-06-25 18:25:56 +0400

    updated db_worker.py

--- db_worker.py
+++ db_worker.py
@@ -4,114 +4,141 @@ import sqlite3
 
 import config
 
+
 class User:
-    
-    def __init__(self, chat_id, state = config.States.DEFAULT.value, current_form = None, current_question = None, forms_number = 0):
+    def __init__(self, chat_id, state=config.States.DEFAULT.value, current_form=None, current_question=None):
         self.chat_id = chat_id
         self.state = state
         self.current_form = current_form
         self.current_question = current_question
-        self.forms_number = forms_number
 
-class SQLighter:
 
-    def __init__(self, database):
-        self.database = database
-        self.create_tables_if_not_exist()
-               
-    def open_connection(self):
-        self.connection = sqlite3.connect(self.database)
-        self.cursor = self.connection.cursor()
+class Form:
+    def __init__(self, form_id, creator_chat_id=None, questions_number=None, name=None, description=None, spreadsheet_id=None):
+        self.form_id = form_id
+        self.creator_chat_id = creator_chat_id
+        self.questions_number = questions_number
+        self.name = name
+        self.description = description
+        self.spreadsheet_id = spreadsheet_id
 
-    def create_tables_if_not_exist(self):
-        self.open_connection()
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Users(
-            chat_id int,
-            state int,
-            current_form text,
-            current_question int,
-            forms_number int
-        )""")
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS QMessages(
-            form_id text,
-            question_id int,
-            message_id int,
-            inserted_time timestamp
-        )""")
-        self.cursor.execute("""CREATE TABLE IF NOT EXISTS Answers(
-            chat_id int,
-            form_id text,
-            question_id int,
-            answer_text text
-        )""")
-        self.connection.commit()
-        self.connection.close()
 
-    def insert_user(self, user):
-        self.open_connection()
-        self.cursor.execute("INSERT INTO Users(chat_id, state, current_form, current_question, forms_number) VALUES(?, ?, ?, ?, ?)", (user.chat_id, user.state, user.current_form, user.current_question, user.forms_number))
-        self.connection.commit()
-        self.connection.close()
+class DataBase:
+    def __init__(self, db_location=config.DB_LOCATION):
+        self.__DB_LOCATION = db_location
+        self.__db_connection = sqlite3.connect(self.__DB_LOCATION)
+        self.__db_cursor = self.__db_connection.cursor()
+        self.__create_table_if_not_exists()
+    
+    def __del__(self):
+        self.__db_connection.close()
 
-    def delete_user(self, chat_id):
-        self.open_connection()
-        self.cursor.execute("DELETE FROM Users WHERE chat_id=?", (chat_id,))
-        self.connection.commit()
-        self.connection.close()
-
-    def get_user(self, chat_id, insert_if_not_exists=True):
-        self.open_connection()
-        users_by_chat_id = self.cursor.execute("SELECT chat_id, state, current_form, current_question, forms_number FROM Users WHERE chat_id = ?", (chat_id,)).fetchall()
-        self.connection.close()
-
-        if not users_by_chat_id:
-            if insert_if_not_exists:
-                user = User(chat_id=chat_id)
-                self.insert_user(user)
-                return user
-            else:
-                return None
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, ext_type, exc_value, traceback):
+        self.__db_cursor.close()
+        if isinstance(exc_value, Exception):
+            self.__db_connection.rollback()
         else:
-            if len(users_by_chat_id) > 1:
-                logging.error("More than one user with one chat_id in database")
-            user_tuple = users_by_chat_id[0]
-            user = User(chat_id=user_tuple[0], state=user_tuple[1], current_form=user_tuple[2], current_question=user_tuple[3], forms_number=user_tuple[4])
-            return user
+            self.__db_connection.commit()
+        self.__db_connection.close()
+    
+    def close(self):
+        self.__db_connection.close()
 
-    def update_user(self, user):
-        self.delete_user(user.chat_id)
-        self.insert_user(user)
+    def commit(self):
+        self.__db_connection.commit()
+    
+    def __create_table_if_not_exists(self):
+        self.__db_cursor.execute("""CREATE TABLE IF NOT EXISTS Users(
+            chat_id INTEGER NOT NULL UNIQUE,
+            state INTEGER,
+            current_form TEXT,
+            current_question INTEGER
+        )""")
+        self.__db_cursor.execute("""CREATE TABLE IF NOT EXISTS Forms(
+            form_id TEXT NOT NULL UNIQUE,
+            creator_chat_id INTEGER,
+            questions_number INTEGER,
+            name TEXT,
+            description TEXT,
+            spreadsheet_id INTEGER
+        )""")
+        self.__db_cursor.execute("""CREATE TABLE IF NOT EXISTS QMessages(
+            form_id TEXT NOT NULL,
+            question_id INTEGER NOT NULL,
+            message_id INTEGER NOT NULL,
+            inserted_time TIMESTAMP NOT NULL
+        )""")
+        self.__db_cursor.execute("""CREATE TABLE IF NOT EXISTS Answers(
+            chat_id INTEGER NOT NULL,
+            form_id TEXT NOT NULL,
+            question_id INTEGER NOT NULL,
+            answer_text TEXT NOT NULL
+        )""")
+        self.commit()
+
+    def __insert_user_if_not_in_db(self, chat_id):
+        self.__db_cursor.execute("INSERT OR IGNORE INTO Users(chat_id) VALUES(?)", (chat_id,))
+    
+    def __insert_form_if_not_in_db(self, form_id):
+        self.__db_cursor.execute("INSERT OR IGNORE INTO Forms(form_id) VALUES(?)", (form_id,))
 
     def insert_message_to_question(self, form_id, question_id, message_id):
-        self.open_connection()
-        self.cursor.execute("INSERT INTO QMessages(form_id, question_id, message_id, inserted_time) VALUES(?, ?, ?, ?)", (form_id, question_id, message_id, datetime.now()))
-        self.connection.commit()
-        self.connection.close()
+        self.__db_cursor.execute("INSERT INTO QMessages(form_id, question_id, message_id, inserted_time) VALUES(?, ?, ?, ?)", (form_id, question_id, message_id, datetime.now()))
+        self.commit()
 
     def get_messages_id_from_question(self, form_id, question_id):
-        self.open_connection()
-        messages = [int(x[0]) for x in self.cursor.execute("SELECT message_id FROM QMessages WHERE form_id=? and question_id=? ORDER BY inserted_time", (form_id, question_id)).fetchall()]
-        self.connection.close()
-        return messages
+        self.__db_cursor.execute("SELECT message_id FROM QMessages WHERE form_id=? and question_id=? ORDER BY inserted_time", (form_id, question_id))
+        items = self.__db_cursor.fetchall()
+        messages_id = []
+        for item in items:
+            messages_id.append(item[0])
+        return messages_id
 
     def insert_answer(self, chat_id, form_id, question_id, answer_text):
-        self.open_connection()
-        self.cursor.execute("INSERT INTO Answers(chat_id, form_id, question_id, answer_text) VALUES(?, ?, ?, ?)", (chat_id, form_id, question_id, answer_text,))
-        self.connection.commit()
-        self.connection.close()
+        self.__db_cursor.execute("INSERT INTO Answers(chat_id, form_id, question_id, answer_text) VALUES(?, ?, ?, ?)", (chat_id, form_id, question_id, answer_text,))
+        self.commit()
+    
+    def get_user(self, chat_id):
+        self.__insert_user_if_not_in_db(chat_id)
+        self.__db_cursor.execute("SELECT state, current_form, current_question FROM Users WHERE chat_id=?", (chat_id,))
+        items = self.__db_cursor.fetchall()
+        state, current_form, current_question = items[0]
+        return User(chat_id=chat_id, state=state, current_form=current_form, current_question=current_question)
 
-    # Bad codestyle
+    def update_user(self, user: User):
+        self.__insert_user_if_not_in_db(user.chat_id)
+        self.__db_cursor.execute("UPDATE Users SET state=?, current_form=?, current_question=? WHERE chat_id=?", (user.state, user.current_form, user.current_question, user.chat_id))
+        self.commit()
+    
+    def is_form_id_free(self, form_id):
+        self.__db_cursor.execute("SELECT form_id FROM Forms WHERE form_id=?", (form_id,))
+        items = self.__db_cursor.fetchall()
+        return len(items) == 0
+    
+    def get_form(self, form_id):
+        self.__insert_form_if_not_in_db(form_id)
+        self.__db_cursor.execute("SELECT creator_chat_id, questions_number, name, description, spreadsheet_id FROM Forms WHERE form_id=?", (form_id,))
+        items = self.__db_cursor.fetchall()
+        creator_chat_id, questions_number, name, description, spreadsheet_id = items[0]
+        return Form(form_id=form_id, creator_chat_id=creator_chat_id, questions_number=questions_number, name=name, description=description, spreadsheet_id=spreadsheet_id)
+
+    def update_form(self, form: Form):
+        self.__insert_form_if_not_in_db(form.form_id)
+        self.__db_cursor.execute("UPDATE Forms SET creator_chat_id=?, questions_number=?, name=?, description=?, spreadsheet_id=? WHERE form_id=?", (form.creator_chat_id, form.questions_number, form.name, form.description, form.spreadsheet_id, form.form_id))
+        self.commit()
+    
     def get_all_answers(self, form_id):
-        self.open_connection()
-        temp = self.cursor.execute("SELECT chat_id, question_id, answer_text FROM Answers WHERE form_id=?", (form_id,)).fetchall()
-        length = 0
-        for x in temp:
-            length = max(length, x[1] + 1)
-
-        answers = []
-        for x in temp:
-            if x[1] == 0:
-                answers.append([])
-            answers[len(answers) - 1].append(x[2])
-        self.connection.close()
-        return answers
+        self.__db_cursor.execute("SELECT chat_id, question_id, answer_text FROM Answers WHERE form_id=?", (form_id,))
+        items = self.__db_cursor.fetchall()
+        form = self.get_form(form_id)
+        assert type(form.questions_number) is int
+        result = dict()
+        for item in items:
+            chat_id, question_id, answer_text = item
+            if chat_id not in result:
+                result[chat_id] = [None] * form.questions_number
+            result[chat_id][question_id] = answer_text
+        return result 
